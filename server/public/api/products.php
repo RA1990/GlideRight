f9e254c2a4e0de5e09c595f490690e7a1c37539c
RA1990 <felx90@outlook.com>
2019-08-22 22:14:14 +0000

    uncommented code from product.php

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -3,13 +3,14 @@ require_once('./function.php');
 set_exception_handler('error_handler');
 $output = file_get_contents('dummy-products-list.json');
 print($output);
-// header('Content-Type: application/json');
 
-// if (empty($_GET['id'])) {
-//   readfile('dummy-products-list.json');
-// } else {
-//   readfile('dummy-product-details.json');
-// }
+header('Content-Type: application/json');
+
+if (empty($_GET['id'])) {
+  readfile('dummy-products-list.json');
+} else {
+  readfile('dummy-product-details.json');
+}
 
 
 ?>
