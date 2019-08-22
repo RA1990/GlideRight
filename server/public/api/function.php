1ae31ada82294f7023f16bf614147cfc3a00c768
RA1990 <felx90@outlook.com>
2019-08-22 23:08:21 +0000

    moved header function to top of product.php file

--- server/public/api/function.php
+++ server/public/api/function.php
@@ -6,13 +6,9 @@ function error_handler($error){
     "success" => false,
     "error" => $error->getMessage()
   );
-   http_response_code(500);
+  http_response_code(500);
   $json_output = json_encode($output);
   print($json_output);
-
 }
-
-
-
 
 ?>
