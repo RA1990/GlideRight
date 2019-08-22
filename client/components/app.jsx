30abfca75423b41ca4012b3a0591c9802267c03c
RA1990 <felx90@outlook.com>
2019-08-22 19:32:37 +0000

    Complete the implementation of ProductDetails's render method to conditionally render a view of the product's details (as soon as the details are loaded from the server).

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './header';
 import ProductList from './productlist';
+import ProductDetails from './product-details';
 
 export default class App extends React.Component {
   render() {
     return (
       <React.Fragment>
         <Header/>
+        <ProductDetails/>
         <ProductList />
       </React.Fragment>
     );
--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -9,9 +9,10 @@ class ProductDetails extends React.Component {
   }
 
   componentDidMount() {
-    // fetch("http://api/products.php?id=1");
-    //  .then(res => res.json());
-
+    // fetch("/api/products.php?id=1")
+    //   .then(res => res.json())
+    //   .then(res => this.setState({product:res}))
+    //   .then(res => console.log(this.state));
   }
 
   render() {
