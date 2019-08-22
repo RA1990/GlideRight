50ee9cd1684194132c7a18e65e29d5ac885a88e2
RA1990 <felx90@outlook.com>
2019-08-22 22:56:46 +0000

    test

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -9,24 +9,24 @@ class ProductDetails extends React.Component {
   }
 
   componentDidMount() {
-    // fetch('/api/products.php?id=1')
-    //   .then(res => res.json())
-    //   .then(res => this.setState({ product: res }))
-    //   .then(res => console.log(this.state));
+    fetch('/api/products.php?id=1')
+      .then(res => res.json())
+      .then(res => this.setState({ product: res }));
   }
 
   render() {
 
     return (
       <div className="card p-3">
-        <img src={this.state.product} className="card-img-top" alt="item1" />
+        <img src={this.state.product.image} className="card-img-top" alt="item1" />
         <div className="card-body">
-          <h5 className="card-title">{this.state.name}</h5>
-          <p className="card-text"><small className="text-muted">{this.state.price}</small></p>
-          <p className="card-text">{this.state.shortDescription}</p>
+          <h5 className="card-title">{this.state.product.name}</h5>
+          <p className="card-text"><small className="text-muted">{this.state.product.price}</small></p>
+          <p className="card-text">{this.state.product.shortDescription}</p>
         </div>
       </div>
     );
+
   }
 
 }
