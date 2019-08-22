43047cb4131ba01f5fcf4c13f024942e0b326173
RA1990 <felx90@outlook.com>
2019-08-22 18:25:29 +0000

    Make initial products endpoint and deliver dummy data

--- server/public/api/function.php
+++ server/public/api/function.php
@@ -1,18 +1,16 @@
 <?php
+
 function error_handler($error){
-$output=array(
- "success"=>false,
- "error"=> $error->getMeassage()
-);
-echo json_encode($output);
-print($json_output);
 
-
-
+  $output = array(
+    "success" => false,
+    "error" => $error->getMessage()
+  );
+   http_response_code(500);
+  $json_output = json_encode($output);
+  print($json_output);
 
 }
-
-
 
 
 
