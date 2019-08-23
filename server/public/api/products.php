8bbb81b95bfd9752b4d86c3116194bb51b6f1355
RA1990 <felx90@outlook.com>
2019-08-23 00:23:50 +0000

    create connection file for mysql

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -2,7 +2,11 @@
 
 header('Content-Type: application/json');
 require_once('./function.php');
+require_once('./db_connection.php');
 set_exception_handler('error_handler');
+if(!$conn) {
+        throw new Exception("error:" . mysqli_connect_error());
+      }
 $output = file_get_contents('dummy-products-list.json');
 print($output);
 
