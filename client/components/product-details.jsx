55ca1b5d7bf1a484a7c9325efd09d89d5da24cd9
RA1990 <felx90@outlook.com>
2019-08-23 15:08:03 +0000

    test

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -9,24 +9,31 @@ class ProductDetails extends React.Component {
   }
 
   componentDidMount() {
-    fetch('/api/products.php?id=1')
+    fetch('/api/products.php?id=')
       .then(res => res.json())
       .then(res => this.setState({ product: res }));
   }
 
   render() {
+    // console.log(this.state.product);
 
-    return (
-      <div className="card p-3">
-        <img src={this.state.product.image} className="card-img-top" alt="item1" />
-        <div className="card-body">
-          <h5 className="card-title">{this.state.product.name}</h5>
-          <p className="card-text"><small className="text-muted">{this.state.product.price}</small></p>
-          <p className="card-text">{this.state.product.shortDescription}</p>
-        </div>
-      </div>
-    );
-
+    // if (this.state.product != null) {
+    //   this.state.product.map(key => {
+    //     key;
+    //     console.log(key.name);
+    //     return (
+    //       <div key={key.id} className="card p-3">
+    //         <img src={key.image} className="card-img-top" alt="item1" />
+    //         <div className="card-body">
+    //           <h5 className="card-title">{key.name}</h5>
+    //           <p className="card-text"><small className="text-muted">{key.price}</small></p>
+    //           <p className="card-text">{key.shortDescription}</p>
+    //         </div>
+    //       </div>
+    //     );
+    //   });
+    // }
+    return null;
   }
 
 }
