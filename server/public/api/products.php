8100bf92ea386e44bf85db86b0bac4e5b9a157c0
RA1990 <felx90@outlook.com>
2019-08-23 01:24:17 +0000

    create schema, table, and fill with data

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -3,6 +3,14 @@
 header('Content-Type: application/json');
 require_once('./function.php');
 require_once('./db_connection.php');
+$query = "SELECT * FROM `wicked`";
+$result = $conn->query($query);
+$data =[];
+while($row = mysqli_fetch_assoc($result)){
+  $data[]=$row;
+};
+print(json_encode($data));
+
 set_exception_handler('error_handler');
 if(!$conn) {
         throw new Exception("error:" . mysqli_connect_error());
