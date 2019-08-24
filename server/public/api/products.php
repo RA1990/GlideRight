dac35fca642c66b5d26f62de44dfe0fa51de5651
RA1990 <felx90@outlook.com>
2019-08-24 16:39:34 +0000

    OUTPUT with id and without id

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -5,7 +5,12 @@ require_once('./function.php');
 set_exception_handler('error_handler');
 startup();
 require_once('./db_connection.php');
-$query = "SELECT * FROM `wicked`";
+if(!empty($_GET['id'])){
+  $whereClause = "WHERE `id`=" . $_GET['id'];
+}else{
+  $whereClause = "";
+};
+$query = "SELECT * FROM `wicked`" . $whereClause;
 $result = $conn->query($query);
 if (!$result) {
   throw new Exception("error:" . mysqli_connect_error());
