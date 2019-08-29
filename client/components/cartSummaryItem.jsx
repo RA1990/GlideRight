5a3ebc05fc6d549c4ffd475ec2a7403c00381f35
RA1990 <felx90@outlook.com>
2019-08-29 16:58:13 +0000

    Allow the user to return to the catalog view from their shopping cart

--- client/components/cartSummaryItem.jsx
+++ client/components/cartSummaryItem.jsx
@@ -3,17 +3,21 @@ import React from 'react';
 function CartSummaryItem(props) {
 
   return (
-    <React.Fragment>
-      <div className="card p-3">
-        <img src={props.image} className="card-img-top" alt="item1" />
-        <div className="card-body">
+
+    <div className="container border">
+      <div className="card-body row ">
+        <div className="col-12 col-md-3">
+          <img src={props.image} className="card-img summaryImg" alt="item" />
+        </div>
+        <div className="col-6 col-md-4 short">
           <h5 className="card-title">{props.name}</h5>
+          <p className="card-text badge badge-primary">{(props.price / 100).toFixed(2)}</p>
           <p className="card-text">{props.shortDescription}</p>
-          <p className="card-text"><span className="badge badge-primary" >${(props.price / 100).toFixed(2)}</span></p>
         </div>
       </div>
-    </React.Fragment>
+    </div>
   );
+
 }
 
 export default CartSummaryItem;
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -7,7 +7,7 @@ function Header(props) {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
         <div><img className="frog" src="https://cdn.pixabay.com/photo/2016/01/29/22/04/frog-1168751_960_720.jpg"/></div>
         <div className='title'>Frog Sales</div>
-        <div className="cart"><div className="cartcount">{props.cartItemCount}</div>&#128722;</div>
+        <div className="cart"><div className="cartcount" onClick={() => props.setView('cart', {})}>{props.cartItemCount}</div>&#128722;</div>
       </nav>
     </React.Fragment>
   );
