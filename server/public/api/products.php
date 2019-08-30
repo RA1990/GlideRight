be04124354d0ea1006ed009a5c2c7f02c14dd439
RA1990 <felx90@outlook.com>
2019-08-30 22:55:46 +0000

    backend six image data

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -13,11 +13,14 @@ if(empty($_GET['id'])){
   $whereClause = "WHERE `wicked`.id=" . $_GET['id'];
 }
 
-$query = "SELECT wicked.id,wicked.name,wicked.price,wicked.shortDescription,
-images.url
-FROM `wicked`
-JOIN `images`
-ON wicked.id=images.productId " . $whereClause;
+$query = "SELECT wicked.id, wicked.name, wicked.price, wicked.shortDescription,
+GROUP_CONCAT(images.url) AS  images
+FROM  wicked
+JOIN  images
+ON wicked.id = images.productId
+$whereClause
+GROUP BY wicked.id";
+
 $result = $conn->query($query);
 if (!$result) {
   throw new Exception("error:" . mysqli_connect_error());
@@ -25,10 +28,12 @@ if (!$result) {
 if(mysqli_num_rows($result) === 0){
   throw new Exception("invalid ID:" . $_GET['id']);
 }
-$data = [];
+
+$output = [];
 while ($row = mysqli_fetch_assoc($result)) {
-  $data[] = $row;
+  $row['images'] = explode(",", $row['images']);
+  $output[] = $row;
 };
-print(json_encode($data));
+print(json_encode($output));
 
 ?>
