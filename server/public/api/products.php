3a04f83ae6d3a5a362f2d04466f1232454c353ca
RA1990 <felx90@outlook.com>
2019-10-01 19:32:06 +0000

    test

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,11 +1,22 @@
 <?php
-
+require_once('db_connection.php');
 header('Content-Type: application/json');
 
 if (empty($_GET['id'])) {
   readfile('dummy-products-list.json');
 } else {
-  readfile('dummy-product-details.json');
+  // readfile('dummy-product-details.json' . $_GET['id']);
+  $query = "SELECT * FROM `wicked`
+  WHERE id =" . $_GET['id'];
+  $result = mysqli_query($conn, $query);
+  if (!$result) {
+    throw new Exception('mysql error ' . mysqli_error($conn));
+  }
+  $data = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $data[] = $row;
+  }
+  print(json_encode($data));
 }
 
 ?>
